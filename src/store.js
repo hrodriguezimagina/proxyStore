ea90f376bae59c5bebc8b2de063a0e4c2625519c
Henry Rodriguez <139818140+hrodriguezimagina@users.noreply.github.com>
2023-12-28 10:32:00 -0500

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,18 +1,4 @@
-import { reactive } from "vue";
-
-function defineStore (data = {}, methods) {
-	const state = reactive(data)
-	 const store = new Proxy(state, {
-		get: (state, prop) => (typeof state[prop] === 'function') ?	(args) =>  ( args ? state[prop](args, data) : state[prop](data) ) : state[prop], 
-		set: (state, prop, value) => state[prop] = value		
-	});
-
-	return new Proxy(store, {
-		apply: function(store, thisArg, argumentList) {
-		 return store['thisArg'](argumentList,  state)
-		}
-	})
-}
+import defineStore from "./defineStore"
 
 const store = {
 	count: 0, 
@@ -22,6 +8,5 @@ const methods = {
 	getName: (state) => 'my name is :'+ state.user.name,
 	getFullName: (args, state) => state.user.name + args.separator + args.lastname	
 }
-
 
 export default defineStore({...store, ...methods})
