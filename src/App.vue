4093e373cbe6e4a2d303651965bb6db6d91b393d
Henry Rodriguez <139818140+hrodriguezimagina@users.noreply.github.com>
2023-12-28 09:59:04 -0500

    Update App.vue
    - updated to use arguments

--- src/App.vue
+++ src/App.vue
@@ -8,24 +8,24 @@ const msg = ref('Store Test')
 watch(
   () => store.user.name,
   (name) => {
-    msg.value = 'name has changed' + name
+    msg.value = 'name has changed: ' + name
   }
 )
 
 
 function add(){
   store.count = store.count + 1
-  store.user.name = 'juanito'
-  store.doubleCounter()
+  store.user.name = 'juanito'  
 }
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-  <p>{{ store.count }}</p>
-  <p>{{ store.counterMinus() }}</p>  
-  <p>{{ store.getName() }}</p>  
+  <h1>{{ msg }}</h1>  
+  <p> state.count : {{ store.count }}</p>
+  <p> getName() : {{ store.getName() }}</p>  
+  <p> function with params : {{ store.getFullName({lastname: 'rodriguez', separator: '//'}) }}</p>  
   <button @click="add()">
     +1 
   </button>
+  
 </template>
