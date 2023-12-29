2ade09ae27aff15695748ef2d8105aeb31cad380
Henry Rodriguez <139818140+hrodriguezimagina@users.noreply.github.com>
2023-12-29 16:59:43 -0500

    added : store.state, store.reset()
    * reset only works on first level props..

--- src/defineStore.js
+++ src/defineStore.js
@@ -1,11 +1,18 @@
 import { reactive } from "vue";
 
 export default function defineStore (data) {
-	const state = reactive(data)
-	 const store = new Proxy(state, {
-		get: (state, prop) => (typeof state[prop] === 'function') ?	(args) =>  ( args ? state[prop](args, data) : state[prop](data) ) : state[prop], 
-		set: (state, prop, value) => state[prop] = value		
+	
+	const initialState = data;
+	let state = reactive({...data})
+	const store = new Proxy(state, {
+		get: (state, prop) => (typeof state[prop] === 'function') ?	(args) =>  ( args ? state[prop](args, state) : state[prop](state) ) : ( prop == 'state' ? state : state[prop]),
+		set: function (state, prop, value) {
+			console.dir({ 'prop': prop, 'value': value})
+			return state[prop] = value 
+		} 		
 	});
+
+	store.reset = () => Object.assign(state, initialState)
 
 	return new Proxy(store, {
 		apply: function(store, thisArg, argumentList) {
