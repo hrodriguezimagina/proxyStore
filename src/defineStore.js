c538a9c38cf97d0a536404df986cfabc53c28310
Henry Rodriguez <139818140+hrodriguezimagina@users.noreply.github.com>
2024-01-02 16:18:36 -0500

    cache Update defineStore.js

--- src/defineStore.js
+++ src/defineStore.js
@@ -1,22 +1,56 @@
 import { reactive } from "vue";
+import cache from '@imagina/qsite/_plugins/cache'
+
+const config = {
+	cache: true,
+	id: 'proxy-store'
+}
+
+let state;
+let store;
+
+function returnFunction(target, prop, args = false){
+	return args ? target[prop](args, state) : target[prop](state)
+}
+
+function returnProp(target, prop){
+	return ( prop == 'state' ? state : target[prop])
+}
+
+function getValue(target, prop){
+	return (typeof target[prop] === 'function') ? (args) =>  returnFunction(target, prop, args) : returnProp(target, prop)
+}
+
+function setValue(target, prop, value){
+	target[prop] = value; 
+	setCache(target, prop, value)
+	return true
+}
+
+function setCache(target, prop, value){
+	if (typeof target[prop] !== 'function') {
+		if( state.hasOwnProperty(prop)){		
+			cache.set('proxy-store', state[prop]);
+		}		
+	}
+}
+
 
 export default function defineStore (data) {
-	
-	const initialState = data;
-	let state = reactive({...data})
-	const store = new Proxy(state, {
-		get: (state, prop) => (typeof state[prop] === 'function') ?	(args) =>  ( args ? state[prop](args, state) : state[prop](state) ) : ( prop == 'state' ? state : state[prop]),
-		set: function (state, prop, value) {
-			console.dir({ 'prop': prop, 'value': value})
-			return state[prop] = value 
-		} 		
-	});
+	state = reactive(data)
 
-	store.reset = () => Object.assign(state, initialState)
+	store = new Proxy(state, {
+		get: (target, prop) => getValue(target, prop),
+		set: (target, prop, value) => setValue(target, prop, value)
+	});	
 
 	return new Proxy(store, {
 		apply: function(store, thisArg, argumentList) {
-		 return store['thisArg'](argumentList,  state)
+		 return store['thisArg'](argumentList, state)
 		}
 	})
 }
+
+
+
+ 
